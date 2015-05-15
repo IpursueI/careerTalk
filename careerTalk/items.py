59a5fd3ea7a164fd184003b832d7f359e4276e44
huangpengjiang <pengjianghuang@163.com>
2015-05-15 12:51:44 +0800

    add .gitignore items.py

--- careerTalk/items.py
+++ careerTalk/items.py
@@ -12,3 +12,11 @@ class CareertalkItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class NJUItem(scrapy.Item):      #南京大学
+    university = scrapy.Field()  #学校名称
+    issueTime = scrapy.Field()   #宣讲会消息发布时间
+    startTime = scrapy.Field()   #宣讲会开始时间
+    location = scrapy.Field()    #宣讲会地点
+    infoSource = scrapy.Field()  #宣讲会信息来源
+    infoDetail = scrapy.Field()  #宣讲会详细信息
