0570aa6449417b81f162dabdefe0fe991fa62a5b
phk52 <phk@zju.edu.cn>
2015-05-15 13:19:14 +0800

    change items.py

--- careerTalk/items.py
+++ careerTalk/items.py
@@ -12,3 +12,15 @@ class CareertalkItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class BaseItem(scrapy.Item):
+    university = scrapy.Field()  # 学校名称 中文全称
+    issueTime = scrapy.Field()   # 宣讲会消息发布时间 2015-05-15
+    startTime = scrapy.Field()   # 宣讲会开始时间  2015-05-15 18:30
+    location = scrapy.Field()    # 宣讲会地点
+    infoSource = scrapy.Field()  # 宣讲会信息来源
+
+
+class SEUItem(BaseItem):      # 东南大学
+    infoDetail = scrapy.Field()  # 宣讲会详细信息
