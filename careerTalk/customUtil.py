5a4b9dba966aefbbe8449ba47a06842212e93198
huangpengjiang <pengjianghuang@163.com>
2015-05-16 17:42:09 +0800

    add customUtil.py

---
+++ careerTalk/customUtil.py
@@ -0,0 +1,10 @@
+#-*- coding:utf-8 -*-
+
+class CustomUtil(object):
+    
+    @staticmethod
+    def convertHtmlContent(li, idx=0):
+        if len(li) == 0:
+            return None
+        else:
+            return li[idx].strip()
--- careerTalk/spiders/NJUSpider.py
+++ careerTalk/spiders/NJUSpider.py
@@ -1,15 +1,35 @@
+# -*- coding:utf-8 -*-
+
 import scrapy
 from careerTalk.items import NJUItem
+from careerTalk.customUtil import CustomUtil
+chc = CustomUtil.convertHtmlContent
 
 class NJUSpider(scrapy.spider.Spider):
     name = "NJU"
     start_urls = ["http://job.nju.edu.cn/login/nju/home.jsp?type=zph&pageNow=1"]
 
     def parse(self, response):
-        item = NJUItem()
-        item['university'] = response.xpath("//div[@class='article-lists']/ul/li[1]/span[1]/a/text()").extract()
+        for sel in response.xpath("//div[@class='article-lists']/ul/li"):
+            item = NJUItem()
+            item['university'] = u"南京大学"
+            item['title'] = chc(sel.xpath("span[1]/a/text()").extract())
+            item_t =  chc(sel.xpath("span[2]/text()").extract()).split()
+            item['location'] = item_t[0]
+            item['startTime'] = item_t[1]+' '+item_t[2]
+            item['infoSource'] = u"南京大学就业创业信息网"
 
-    def parse_page2(self, response):
-        pass
+            url = response.url[:response.url.rindex('/')+1] + chc(sel.xpath("span[1]/a/@href").extract())
+            request = scrapy.Request(url,callback=self.parse_detail)
+            request.meta['item'] = item
+            yield request
 
-
+    def parse_detail(self, response):
+        item = response.meta['item']
+        stmp = chc(response.xpath("//div[@class='article-info']").extract())
+        start = stmp.find("</span>")
+        start = stmp.find("</span>",start+1)
+        end = stmp.find("<span",start)
+        item['issueTime'] = stmp[start+7:end].strip()
+        #item['infoDetail'] = response.xpath("//table[@class='job_detail']").extract()[0].strip()
+        yield item 
