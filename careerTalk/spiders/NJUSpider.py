e6ca31a23787223b256dfdde9ff117a740f704e7
huangpengjiang <pengjianghuang@163.com>
2015-05-16 09:44:15 +0800

    delete NJUSpider.py

--- careerTalk/spiders/NJUSpider.py
+++
@@ -1,8 +0,0 @@
-import scrapy
-
-class NJUSpider(scrapy.spider.Spider):
-    name = "NJU"
-    start_urls = [http://job.nju.edu.cn/login/nju/home.jsp?type=zph&pageNow=1]
-
-    def parse(self, response):
-        
